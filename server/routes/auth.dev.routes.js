651975d6d84a7177298c26882bd4e9f3d465206a
Kajal <banurajpootdev@gmail.com>
2026-08-26 13:44:55 +0530

    bypass changes

--- server/routes/auth.dev.routes.js
+++ server/routes/auth.dev.routes.js
@@ -1,9 +1,26 @@
 import express from "express";
-import { sendOtp, verifyOtp } from "../controllers/auth.controller.js";
+
+import {
+    sendOtp,
+    verifyOtp,
+    deleteAccount
+} from "../controllers/auth.controller.js";
+
+import { authenticate } from "../middleware/auth.js";
 
 const router = express.Router();
 
+// Send OTP
 router.post("/send-otp", sendOtp);
+
+// Verify OTP
 router.post("/verify-otp", verifyOtp);
+
+// Delete Account
+router.delete(
+    "/delete-account",
+    authenticate,
+    deleteAccount
+);
 
 export default router;
